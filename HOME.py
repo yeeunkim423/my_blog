e1bd5d2e27662dfdbb35ee94c93f7e3c60cebd21
yeeunk <mirr050423@gmail.com>
2025-05-14 14:20:08 +0900

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -11,6 +11,8 @@ st.header("Welcome to my project page!")
 with st.expander("Undress - Sombr"):
     st.write("""
     And I don't wanna learn another scent.
+
+    
     I don't want children of another man to have the eyes of the girl I won't forget.
     """)
     st.markdown("*Tags: #teamwork*")
