25a4dc06601160af6f76865d813b0a746d25dee3
yeeunk <mirr050423@gmail.com>
2025-05-14 14:46:04 +0900

    Update Vocabulary.py

--- pages/Vocabulary.py
+++ pages/Vocabulary.py
@@ -6,7 +6,7 @@ import random
 
 st.write("🌱 Vocabulary learning")
 
-tab1, tab2, tab3, tab4 = st.tabs(["❄️ 1. Lesson: Word list", "❄️ 2. Activity: Listen to the word", "❄️ 3. Spelling practice", "❄️ 4. This is Tab4"])
+tab1, tab2, tab3, tab4 = st.tabs(["❄️ 1. Lesson: Word list", "❄️ 2. Activity: Listen to the word", "❄️ 3. Spelling practice", "❄️ 4. TBA"])
 
 ######### TAB 1
 
@@ -99,3 +99,5 @@ with tab3:
             st.error("❌ Try again.")
 
 ######### TAB 4
+with tab4:
+  st.caption("나중에 만들게 :)")
