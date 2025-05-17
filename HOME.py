a9de2d6b9af14c5b82e08461e90c7258996d01cd
yeeunkim423 <yeeunkim423@naver.com>
2025-05-17 14:00:08 +0900

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -4,56 +4,7 @@ st.title("🍃 Comfort Place for Daelicent")
 st.caption("This is my comfort place for Daelicent")
 
 url = "https://github.com/yeeunkim423/my_blog/raw/main/images/alicent-daemon.jpg"
-st.image(url, caption="The look of love", use_container_width=True)
+st.image(url, caption="1X01 The Heirs of the Dragon", use_container_width=True)
 
 st.header("Welcome to my page!")
-
-st.header("Daelicent playlist")
-
-with st.expander("Lover, you should've come over - Jeff Buckey"):
-    st.write("""
-   So I'll wait for you, love.  
-   And I'll burn.  
-   Will I ever see your sweet return?  
-   Oh, will I ever learn?  
-   Oh-oh, lover, you should've come over.  
-   'Cause it's not too late.  
-    """)
-    st.video("https://youtu.be/HxfE6PJmGS8?si=hDiT753U7Jjsmj0V")  
-
-with st.expander("Undress - Sombr"):
-    st.write("""
-    And I don't wanna learn another scent.  
-    I don't want children of another man to have the eyes of the girl I won't forget.
-    """)
-    st.video("https://youtu.be/fOQ_-gZsnYQ?si=vuIbLiNaLJvInesH")  
-    
-with st.expander("the 1 - Taylor Swift"):
-    st.write("""
-    And if my wishes came true, it would've been you.  
-    In my defense, I have none.  
-    For never leaving well enough alone.  
-    But it would've been fun, if you would've been the one.
-    """)
-    st.video("https://youtu.be/KsZ6tROaVOQ?si=go9LzboK4SXqExIQ")  
-
-with st.expander("Dark Paradise - Lana Del Rey"):
-    st.write("""
-    And there's no remedy for memory, your face is like a melody.  
-    It won't leave my head.  
-    Your soul is haunting me and telling me that everything is fine.  
-    But I wish I was dead (dead, like you)  
-    """)
-    st.video("https://youtu.be/dvSZQ4oMHGM?si=lqs_eydmSPLaampU")  
-
-with st.expander("The night we met - Lord Huron"):
-    st.write("""
-    I had all and then most of you  
-    Some and now none of you.  
-    Take me back to the night we met.  
-    I don't know what I'm supposed to do,  
-    Haunted by the ghost of you.  
-    Oh, take me back to the night we met.
-    """)
-    st.video("https://youtu.be/KtlgYxa6BMU?si=CknybMM_lOHocsqa")
 
