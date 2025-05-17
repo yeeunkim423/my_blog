bec5db2a414e0dd800277ec64c8d415bc9080526
yeeunkim423 <yeeunkim423@naver.com>
2025-05-17 12:11:27 +0900

    Update About.py

--- pages/About.py
+++ pages/About.py
@@ -1,10 +1,5 @@
 import streamlit as st
 
-st.write("This app is under construction.")
-
-url="https://github.com/yeeunk28/streamlit25/raw/main/images/image01.png"
-st.image(url, caption="Image link", width=300)  # width in pixels
-
 def fanmix_page():
     st.title("🎧 Daelicent Fanmix")
     st.caption("A soundtrack for love, ruin, and everything in between.")
