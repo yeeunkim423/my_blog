3ed7d9bc045a781f9049bf44f3fe2d0a56c5fb89
yeeunkim423 <yeeunkim423@naver.com>
2025-05-17 14:10:13 +0900

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -8,14 +8,4 @@ st.image(url, caption="1X01 The Heirs of the Dragon", use_container_width=True)
 
 st.header("Welcome to my page!")
 
-st.subheader("Characters")
-st.write("""
-**Daemon Targaryen**  
-- The charismatic and complex CEO  
-- Known for his boldness and charm
-
-**Alicent Hightower**  
-- Influencer and daughter of a powerful politician  
-- Balances public image and private desires
-""")
 
