325a88f627268f3620969f0ce0204a2ccfbe4ae4
yeeunkim423 <yeeunkim423@naver.com>
2025-05-18 22:38:13 +0900

    Update About.py

--- pages/About.py
+++ pages/About.py
@@ -6,12 +6,12 @@ st.title("Daelicent 소개")
 st.caption("House of the Dragon 드라마의 알리센트 하이터워와 다에몬 타르가르옌 캐릭터 소개")
 
 # 알리센트 소개
-with st.expander("알리센트 하이터워"):
+with st.expander("알리센트 하이타워"):
     st.image("https://upload.wikimedia.org/wikipedia/en/9/98/Alicent_Hightower_House_of_the_Dragon.jpg",
-             caption="알리센트 하이터워 (House of the Dragon)", use_container_width=True)
+             caption="알리센트 하이타워 (House of the Dragon)", use_container_width=True)
     st.markdown("""
-    **배우:** 에밀리 케리(어린 시절), 올리비아 쿡(성인 시절)  
-    **역할:** 오토 하이터워의 딸, 왕 비세리스 1세의 아내, 타르가르옌 가문의 중요한 인물들 어머니  
+    **배우:** 에밀리 캐리(어린 시절), 올리비아 쿡(성인 시절)  
+    **역할:** 오토 하이타워의 딸, 왕 비세리스 1세의 아내, 타르가르옌 가문의 중요한 인물들 어머니  
     **성격:** 야심차고 정치적으로 능숙하며 복잡한 감정을 지닌 인물  
     **스토리 중요성:** 타르가르옌 내전(용들의 춤)에서 핵심적인 역할을 담당
     """)
