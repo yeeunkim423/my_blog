ebe0dfac712d91c1a174675e1863152f45063612
yeeunkim423 <yeeunkim423@naver.com>
2025-05-18 21:43:23 +0900

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -8,4 +8,32 @@ st.image(url, caption="1X01 The Heirs of the Dragon", use_container_width=True)
 
 st.header("Welcome to my page!")
 
+st.title("About Them")
 
+# 각자 SNS 링크 (예시)
+person1 = {
+    "name": "Emily Carey",
+    "Instagram": "https://www.instagram.com/theemilycarey/",
+}
+
+person2 = {
+    "name": "Olivia Cooke",
+    "Instagram": "https://www.instagram.com/livkatecooke/",
+}
+
+person3 = {
+  "name": "Matt Smith",
+}
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.header(person1["name"])
+    st.markdown(f"[📸 Instagram]({person1['Instagram']})")
+
+with col2:
+    st.header(person2["name"])
+    st.markdown(f"[📸 Instagram]({person2['Instagram']})")
+
+with col3:
+  st.header(person3["name"])
